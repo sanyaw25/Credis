300a4c67345cdea139240ee0e57cb8948f05ac40
InfectedMunshroom <h.a.ayushman@gmail.com>
2024-11-09 13:54:55 +0530

    Basic go web server created, login created and home page created using Auth0 Changes to be committed: 	new file:   .env 	new file:   go.mod 	new file:   go.sum 	new file:   main.go 	new file:   platform/authenticator/auth.go 	new file:   platform/middleware/isAuthenticated.go 	new file:   platform/router/router.go 	new file:   web/app/callback/callback.go 	new file:   web/app/login/login.go 	new file:   web/app/logout/logout.go 	new file:   web/app/user/user.go 	new file:   web/static/css/styles.css 	new file:   web/template/home.html 	new file:   web/template/user.html

---
+++ platform/router/router.go
@@ -0,0 +1,43 @@
+// platform/router/router.go
+
+package router
+
+import (
+	"encoding/gob"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	"Credis/platform/authenticator"
+	"Credis/web/app/callback"
+	"Credis/web/app/login"
+	"Credis/web/app/logout"
+	"Credis/web/app/user"
+)
+
+// New registers the routes and returns the router.
+func New(auth *authenticator.Authenticator) *gin.Engine {
+	router := gin.Default()
+
+	// To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
+
+	router.Static("/public", "web/static")
+	router.LoadHTMLGlob("web/template/*")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "home.html", nil)
+	})
+	router.GET("/login", login.Handler(auth))
+	router.GET("/callback", callback.Handler(auth))
+	router.GET("/user", user.Handler)
+	router.GET("/logout", logout.Handler)
+
+	return router
+}
