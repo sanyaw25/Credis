cb247d18b943a7b569d65401218aa243b3c02c09
InfectedMunshroom <h.a.ayushman@gmail.com>
2024-11-09 22:00:43 +0530

    Adding https compatibility Changes to be committed: 	modified:   .env 	modified:   main.go 	new file:   server.crt 	new file:   server.key

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// main.go
-
 package main
 
 import (
@@ -16,6 +14,7 @@ import (
 func main() {
 	db.Connect()
 	defer db.Disconnect()
+
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Failed to load the env vars: %v", err)
 	}
@@ -27,8 +26,21 @@ func main() {
 
 	rtr := router.New(auth)
 
-	log.Print("Server listening on http://localhost:3000/")
-	if err := http.ListenAndServe("0.0.0.0:3000", rtr); err != nil {
-		log.Fatalf("There was an error with the http server: %v", err)
+	// Start an HTTP server to redirect all HTTP requests to HTTPS
+	go func() {
+		httpRouter := http.NewServeMux()
+		httpRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			// Redirect HTTP to HTTPS
+			http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
+		})
+		if err := http.ListenAndServe(":80", httpRouter); err != nil {
+			log.Fatalf("Failed to start HTTP redirect server: %v", err)
+		}
+	}()
+
+	// Start the HTTPS server
+	log.Print("Server listening on https://localhost:443/")
+	if err := rtr.RunTLS(":443", "server.crt", "server.key"); err != nil {
+		log.Fatalf("Failed to start HTTPS server: %v", err)
 	}
 }
